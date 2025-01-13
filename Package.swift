2e13eaa4091499ab37428cee910bf5f6ff9948d9
Paolo Rossignoli <paolo.rossignoli@azero.it>
2025-01-13 11:39:46 +0100

    Change the source directory name

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version: 5.7
 import PackageDescription
 
-let vlcBinary = Target.binaryTarget(name: "VLCKit", url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a9/VLCKit.xcframework.zip", checksum: "62144b9e9c25b349b3b6ffe8ee2f7f8641587e21a0e18078986d975c140bfb6b")
-
 let package = Package(
     name: "VLCKitIPTelevision",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
@@ -12,14 +10,16 @@ let package = Package(
             targets: ["VLCKit"]
         )
     ],
-    dependencies: [],
     targets: [
-        vlcBinary,
+        .binaryTarget(
+            name: "VLCKit",
+            url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a9/VLCKit.xcframework.zip", 
+            checksum: "62144b9e9c25b349b3b6ffe8ee2f7f8641587e21a0e18078986d975c140bfb6b"
+        ),
         .target(
             name: "VLCKitIPTelevision",
-            dependencies: [
-                .target(name: "VLCKit")
-            ], linkerSettings: [
+            dependencies: ["VLCKit"],
+            linkerSettings: [
                 .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
                 .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
                 .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
@@ -36,6 +36,8 @@ let package = Package(
                 .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
                 .linkedFramework("Foundation", .when(platforms: [.macOS])),
                 .linkedLibrary("iconv")
-            ]),
+            ]
+        )
     ]
 )
+
