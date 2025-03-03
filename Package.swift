6e290cafa50cca7ea5acbaa4746394bff0935e67
Paolo Rossignoli <paolo.rossignoli@azero.it>
2025-03-03 23:26:58 +0100

    Test libs from chatgpt

--- Package.swift
+++ Package.swift
@@ -1,43 +1,26 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "VLCKitIPTelevision",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
+    name: "vlckit-iptelevision",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v11),
+        .macOS(.v10_11),
+        .visionOS(.v1),
+        .watchOS(.v7_4)
+    ],
     products: [
         .library(
-            name: "VLCKitIPTelevision",
-            targets: ["VLCKit"]
-        )
+            name: "vlckit-iptelevision",
+            targets: ["vlckit-iptelevision"]
+        ),
     ],
     targets: [
         .binaryTarget(
-            name: "VLCKit",
-            url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a9/VLCKit.xcframework.zip", 
-            checksum: "62144b9e9c25b349b3b6ffe8ee2f7f8641587e21a0e18078986d975c140bfb6b"
-        ),
-        .target(
-            name: "VLCKitIPTelevision",
-            dependencies: ["VLCKit"],
-            linkerSettings: [
-                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
-                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Security", .when(platforms: [.iOS])),
-                .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
-                .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
-                .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Foundation", .when(platforms: [.macOS])),
-                .linkedLibrary("iconv")
-            ]
+            name: "vlckit-iptelevision",
+            url: "https://download.videolan.org/cocoapods/unstable/VLCKit-4.0.0a10-d962e05c-17860384.tar.xz",
+            checksum: "8d3b1f8d1b6e4fc35d45b706a800a424a1b841d2dc5d4229817a3d7a000bf372"
         )
     ]
 )
-
--- Sources/VLCKitIPTelevision/VLCKit.swift
+++
@@ -1,7 +0,0 @@
-#if os(tvOS)
-@_exported import TVVLCKit
-#elseif os(iOS) && !targetEnvironment(macCatalyst)
-@_exported import MobileVLCKit
-#elseif os(macOS)
-@_exported import VLCKit
-#endif
