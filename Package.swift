542d02aa003b98753050e583c18489e697e933a4
Paolo Rossignoli <paolo.rossignoli@azero.it>
2025-03-14 19:25:26 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -1,26 +1,40 @@
 // swift-tools-version:5.5
 import PackageDescription
 
+let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a10/VLCKit.xcframework.zip", checksum: "8add195287f3c5a1a1d6581cf08e2b987c7857814baff819dae4fa071bd3d974")
+
 let package = Package(
-    name: "VLCKitIPTV",
-    platforms: [
-        .macOS(.v10_11),
-        .iOS(.v9),
-        .tvOS(.v11),
-        .watchOS(.v7_4),
-        .visionOS(.v1)
-    ],
+    name: "vlckit-spm",
+    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [
         .library(
-            name: "VLCKitIPTV",
-            targets: ["VLCKit"]
-        )
+            name: "VLCKitSPM",
+            targets: ["VLCKitSPM"]),
     ],
+    dependencies: [],
     targets: [
-        .binaryTarget(
-            name: "VLCKit",
-            url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a10/VLCKit.xcframework.zip",
-            checksum: "8add195287f3c5a1a1d6581cf08e2b987c7857814baff819dae4fa071bd3d974"
-        )
+        vlcBinary,
+        .target(
+            name: "VLCKitSPM",
+            dependencies: [
+                .target(name: "VLCKit-all")
+            ], linkerSettings: [
+                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Security", .when(platforms: [.iOS])),
+                .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
+                .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
+                .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Foundation", .when(platforms: [.macOS])),
+                .linkedLibrary("iconv")
+            ]),
     ]
 )
