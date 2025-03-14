3e955c96c3e77c389a71c080197260a4bc6d1361
Paolo Rossignoli <paolo.rossignoli@azero.it>
2025-03-14 19:36:57 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -1,40 +1,35 @@
-// swift-tools-version:5.5
+// swift-tools-version: 6.0
+
 import PackageDescription
 
-let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a10/VLCKit.xcframework.zip", checksum: "8add195287f3c5a1a1d6581cf08e2b987c7857814baff819dae4fa071bd3d974")
-
 let package = Package(
-    name: "vlckit-spm",
-    platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
-    products: [
-        .library(
-            name: "VLCKitSPM",
-            targets: ["VLCKitSPM"]),
-    ],
-    dependencies: [],
-    targets: [
-        vlcBinary,
-        .target(
-            name: "VLCKitSPM",
-            dependencies: [
-                .target(name: "VLCKit-all")
-            ], linkerSettings: [
-                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
-                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Security", .when(platforms: [.iOS])),
-                .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
-                .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
-                .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
-                .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
-                .linkedFramework("Foundation", .when(platforms: [.macOS])),
-                .linkedLibrary("iconv")
-            ]),
-    ]
+  name: "VLC",
+  platforms: [
+    .iOS(.v17),
+    .tvOS(.v17),
+    .macOS(.v14),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(
+      name: "VLC",
+      targets: [
+        "VLC",
+      ]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "VLC",
+      dependencies: [
+        .target(name: "VLCKit"),
+      ],
+      path: "Sources"
+    ),
+    .binaryTarget(
+      name: "VLCKit",
+      url: "https://github.com/paoloandrea/vckit-package/releases/download/v4.0.0a10/VLCKit.xcframework.zip",
+      checksum: "8add195287f3c5a1a1d6581cf08e2b987c7857814baff819dae4fa071bd3d974"
+    ),
+  ]
 )
